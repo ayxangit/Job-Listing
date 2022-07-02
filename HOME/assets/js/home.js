69ea1ff24d0554c115699134e9ca4f8d241a3b44
ayxangit <ayxanelxanli98@gmail.com>
2022-07-03 01:39:19 +0400

    finish home side

--- HOME/assets/js/home.js
+++ HOME/assets/js/home.js
@@ -1,3 +1,15 @@
+// header change scroll
+let header = document.querySelector(".header")
+
+window.onscroll =()=>{
+    if(window.scrollY>=100){
+        header.classList.add("scroll_change");
+    }else{
+        header.classList.remove("scroll_change");
+    }
+}
+// end
+
 
 // start  first select tag section Job Posted
 let text_icon = document.querySelector("text_icon")
@@ -74,4 +86,31 @@ var swiper2 = new Swiper(".mySwiper_box", {
 });
 //   end
 
+
+// footer click display block span
+let btn = document.querySelector("#btn").addEventListener("click",()=>{
+    let input = document.querySelector("#input").value;
+    if(input ==""){
+        alert("Email ünvanınızı qeyd edin")
+    }else{
+        let span = document.querySelector(".span");
+        span.classList.add("display_block_click");
+    }
+    
+})
+// end
+
+// scroll to top button
+window.addEventListener('scroll',function(){
+    let scroll = document.querySelector(".scroll_to_button");
+    scroll.classList.toggle("scroll_visibilty_opacity",window.scrollY>500);
+})
+
+function scrollToTop(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+}
+// end
 
