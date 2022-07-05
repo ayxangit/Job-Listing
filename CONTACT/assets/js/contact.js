b3877590eee0829df588dc660e843687e4dcab2f
ayxangit <ayxanelxanli98@gmail.com>
2022-07-05 10:30:31 +0400

    end contact page

---
+++ CONTACT/assets/js/contact.js
@@ -0,0 +1,37 @@
+// footer click display block span
+let btn = document.querySelector("#btn").addEventListener("click",()=>{
+    let input = document.querySelector("#input").value;
+    if(input ==""){
+        alert("Email ünvanınızı qeyd edin")
+    }else{
+        let span = document.querySelector(".span");
+        span.classList.add("display_block_click");
+    }
+    
+  })
+  // end
+
+  // scroll to top button
+window.addEventListener('scroll',function(){
+    let scroll = document.querySelector(".scroll_to_button");
+    scroll.classList.toggle("scroll_visibilty_opacity",window.scrollY>500);
+  })
+  
+  function scrollToTop(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+  }
+  // end
+
+let header = document.querySelector(".header")
+
+window.onscroll =()=>{
+    if(window.scrollY>=100){
+        header.classList.add("scroll_change");
+    }else{
+        header.classList.remove("scroll_change");
+    }
+}
+// end
