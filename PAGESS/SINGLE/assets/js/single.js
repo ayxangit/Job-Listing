bdcaa58ad5eff9f0d4268eeb529fb0c392491688
ayxangit <ayxanelxanli98@gmail.com>
2022-07-05 16:53:18 +0400

    end single page

---
+++ PAGESS/SINGLE/assets/js/single.js
@@ -0,0 +1,50 @@
+var swiper2 = new Swiper(".mySwiper_box", {
+    loop:true,
+    autoplay:true,
+    slidesPerView:1,
+    spaceBetween: 30,
+    speed:350,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+//   end
+
+// header change scroll
+let header = document.querySelector(".header")
+
+window.onscroll =()=>{
+    if(window.scrollY>=100){
+        header.classList.add("scroll_change");
+    }else{
+        header.classList.remove("scroll_change");
+    }
+}
+// end
+
+window.addEventListener('scroll',function(){
+    let scroll = document.querySelector(".scroll_to_button");
+    scroll.classList.toggle("scroll_visibilty_opacity",window.scrollY>500);
+})
+
+function scrollToTop(){
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+    })
+}
+// end
+
+// footer click display block span
+let btn = document.querySelector("#btn").addEventListener("click",()=>{
+    let input = document.querySelector("#input").value;
+    if(input ==""){
+        alert("Email ünvanınızı qeyd edin")
+    }else{
+        let span = document.querySelector(".span");
+        span.classList.add("display_block_click");
+    }
+    
+})
+  // end
